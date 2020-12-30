a840a13f852b2feb9210481aad5670d58cf0bdff
Niall Colbert <niallcolbert87@gmail.com>
2020-12-30 13:29:36 +0000

    Build for production

--- config/index.js
+++ config/index.js
@@ -10,6 +10,7 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
+    publicPath: '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
